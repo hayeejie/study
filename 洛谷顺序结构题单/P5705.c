96ea97009e81191f26217a1afd6abae0648f151a
hayee <a1287150696@163.com>
2022-03-10 12:52:58 +0800

    3/10

---
+++ 洛谷顺序结构题单/P5705.c
@@ -0,0 +1,18 @@
+/*
+ * @Author: hayee
+ * @Date: 2022-03-10 12:24:06
+ * @LastEditTime: 2022-03-10 12:45:27
+ * @LastEditors: hayee
+ * @Github: hayeejie
+ * @FilePath: \github\洛谷顺序结构题单\P5705.c
+ * @ProgramDescription: 数字反转
+ * 千山我独行,不必相送
+ */
+#include<stdio.h>
+int main()
+{
+    char a,b,c,d;
+    scanf("%c%c%c.%c",&a,&b,&c,&d);
+    printf("%c.%c%c%c",d,c,b,a);
+    return 0;
+}
